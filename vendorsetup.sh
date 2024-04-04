8dec465932f9c0c6b9b2780e74697b9e19765579
zaidkhan0997 <kzaid0997@gmail.com>
2024-04-04 21:31:42 +0530

    sweet: update vendorsetup.sh
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
-git clone -b 14 --depth=1 https://github.com/sweet-lisa/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
-git clone -b 13.0 --depth=1 https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r498229b.git prebuilts/clang/host/linux-x86/r498229b
+git clone --depth=1 https://github.com/pure-soul-kk/kernel_xiaomi_sweet kernel/xiaomi/sm6150
+git clone --depth=1 https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r498229b.git -b 13.0 prebuilts/clang/host/linux-x86/clang -r498229b
